e6f4b3a193e9d328b56d58552bde15f4c002efc2
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-20 23:57:01 +0700

    modified: Footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="container border-t border-gray-300 py-6 text-center">
+      <h1 className="font-semibold text-gray-900">
+        &copy; Copyrights {new Date().getFullYear()} - Develop by Fajar Fadillah
+        A ❤️
+      </h1>
+    </footer>
+  );
 };
 
 export default Footer;
