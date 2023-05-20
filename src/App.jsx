c30ce2e1ec2e5f8579fc19a1a33c436266f33b9a
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-20 22:45:43 +0700

    create: Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// import pages
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div className="section-title">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
