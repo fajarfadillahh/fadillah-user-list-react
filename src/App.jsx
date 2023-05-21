1dd1d3dc368cff79e18dee1ab65308b3d7eb081c
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-21 22:20:30 +0700

    create: User page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import pages
 import Home from "./pages/Home";
 import About from "./pages/About";
+import User from "./pages/User";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/users" element={<User />} />
       </Routes>
     </Router>
   );
