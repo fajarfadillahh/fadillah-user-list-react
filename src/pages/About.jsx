9b1f9ed14aceef3d60ef77867fdc4089080e987c
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-21 22:14:58 +0700

    create: Hero parts to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,15 @@ import React from "react";
 // import components
 import Layout from "../components/layout/Layout";
 
+// import parts
+import Hero from "../parts/about/Hero";
+
 const About = () => {
-  return <Layout>About page</Layout>;
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  );
 };
 
 export default About;
---
+++ src/parts/about/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="pb-16 pt-32">
+      <div className="container max-w-2xl text-center">
+        <h1 className="section-title pb-4 text-[36px]">About Page</h1>
+        <div className="grid gap-2">
+          <p className="section-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
+            beatae sint quam sit quod facere qui quidem, itaque ad obcaecati est
+            dignissimos eos perferendis, nulla natus voluptatum ut cum! Alias
+            repudiandae vel provident, unde ut eum numquam inventore sint culpa
+            libero.
+          </p>
+          <p className="section-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
+            beatae ex consequuntur laborum sit quod facere qui quidem, itaque ad
+            obcaecati est dignissimos eos perferendis, nulla natus voluptatum ut
+            cum.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
