ded29a599a5d57ef52cfa57a8103d24b18e78267
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-21 22:45:12 +0700

    create: fetching file for get all user data

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-// import components
+// import components & parts
 import Layout from "../components/layout/Layout";
-
-// import parts
 import UserList from "../parts/user/UserList";
 
+// import api
+import { getAllUsers } from "../api/fetching";
+
 const User = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getAllUsers()
+      .then((result) => {
+        setUsers(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log({ data: users });
+
   return (
     <Layout>
       <UserList />
