080f0a07fb1361fa58c0e799a3b899c7ecc2493f
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-21 22:20:44 +0700

    create: User page

---
+++ src/pages/User.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import components
+import Layout from "../components/layout/Layout";
+
+const User = () => {
+  return (
+    <Layout>
+      <div className="pt-40">User page</div>
+    </Layout>
+  );
+};
+
+export default User;
