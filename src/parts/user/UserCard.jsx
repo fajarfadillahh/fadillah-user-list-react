96f6cb5c9a0d75c20e90641b9bb37a06467ffa26
fajarfadillahh <fajaragustian979@gmail.com>
2023-06-01 21:25:29 +0700

    modified: UserList & UserCard parts, responsive for mobile device on UserCard

--- src/parts/user/UserCard.jsx
+++ src/parts/user/UserCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const UserCard = ({ user }) => {
   return (
-    <div className="group relative w-[768px] cursor-pointer overflow-hidden rounded-lg bg-white shadow-md">
+    <div className="group relative cursor-pointer overflow-hidden rounded-lg bg-white shadow-md lg:w-[768px]">
       <div className="absolute left-0 top-0 h-full w-0 bg-purple-600 transition-all duration-300 group-hover:w-[6px]" />
 
       <div className="flex h-[84px] items-center px-12">
--- src/parts/user/UserList.jsx
+++ src/parts/user/UserList.jsx
@@ -7,10 +7,10 @@ import UserCard from "./UserCard";
 const UserList = ({ dataUsers }) => {
   return (
     <section className="pb-16 pt-32">
-      <div className="container grid justify-center gap-8">
+      <div className="container grid gap-8">
         <h1 className="section-title text-center text-[36px]">All Users</h1>
 
-        <div className="grid justify-center gap-4">
+        <div className="grid gap-4 lg:justify-center">
           {dataUsers.map((user, index) => {
             return (
               <Link to={`/users/${user.id}`} key={index}>
