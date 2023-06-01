204ed7546f563524f8f1360aebe26a02523c8274
fajarfadillahh <fajaragustian979@gmail.com>
2023-06-01 21:04:00 +0700

    modified: UserList & Hero parts

--- src/parts/user/UserList.jsx
+++ src/parts/user/UserList.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // import parts
 import UserCard from "./UserCard";
 
 const UserList = ({ dataUsers }) => {
-  console.log(dataUsers);
-
   return (
     <section className="pb-16 pt-32">
       <div className="container grid justify-center gap-8">
         <h1 className="section-title text-center text-[36px]">All Users</h1>
 
         <div className="grid justify-center gap-4">
-          {dataUsers.map((user) => {
-            return <UserCard key={user.id} user={user} />;
+          {dataUsers.map((user, index) => {
+            return (
+              <Link to={`/users/${user.id}`} key={index}>
+                <UserCard user={user} />
+              </Link>
+            );
           })}
         </div>
       </div>
--- src/parts/userid/Hero.jsx
+++ src/parts/userid/Hero.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowLeft } from "react-icons/hi";
 
 const Hero = () => {
-  return <div className="pt-40">Hero</div>;
+  return (
+    <section className="pb-16 pt-32">
+      <div className="container flex items-center">
+        <div className="mx-auto grid w-[768px] gap-8">
+          <Link
+            to="/users"
+            className="inline-flex w-max items-center gap-1 text-gray-900 hover:text-purple-600"
+          >
+            <HiOutlineArrowLeft className="text-[1.1rem]" />
+            <span className="font-semibold">Back to list</span>
+          </Link>
+
+          <div className="grid gap-6">
+            <div className="text-center">
+              <h1 className="sectio-title text-[48px] font-extrabold">
+                Details User 1
+              </h1>
+              <p className="text-[18px] font-bold text-gray-900">
+                Hi, username
+              </p>
+            </div>
+
+            <ul className="flex flex-col gap-2">
+              <li className="inline-flex items-center gap-1 text-gray-900">
+                <p className="font-semibold">Name:</p>
+                <p className="font-semibold">Fajar Fadillah Agustian</p>
+              </li>
+              <li className="inline-flex items-center gap-1 text-gray-900">
+                <p className="font-semibold">Name:</p>
+                <p className="font-semibold">Fajar Fadillah Agustian</p>
+              </li>
+              <li className="inline-flex items-center gap-1 text-gray-900">
+                <p className="font-semibold">Name:</p>
+                <p className="font-semibold">Fajar Fadillah Agustian</p>
+              </li>
+              <li className="inline-flex items-center gap-1 text-gray-900">
+                <p className="font-semibold">Name:</p>
+                <p className="font-semibold">Fajar Fadillah Agustian</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
