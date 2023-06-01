1006d3b99dc6e09874ff4f3b72733927ed36ac73
fajarfadillahh <fajaragustian979@gmail.com>
2023-06-01 20:42:39 +0700

    create: UserId for user detail page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import User from "./pages/User";
+import UserId from "./pages/UserId";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/users" element={<User />} />
+        <Route path="/users/:id" element={<UserId />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/UserId.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserId = () => {
+  return <div>User details</div>;
+};
+
+export default UserId;
