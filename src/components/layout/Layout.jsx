8f516e97741f50f9e015d39d3ba6e639b0d086a0
fajarfadillahh <fajaragustian979@gmail.com>
2023-06-01 21:32:23 +0700

    modified: added scrollup effect at the Layout components

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // import components
 import NavbarMobile from "../navbar/NavbarMobile";
@@ -6,6 +6,11 @@ import NavbarDesktop from "../navbar/NavbarDesktop";
 import Footer from "../footer/Footer";
 
 const Layout = ({ children }) => {
+  // scrollup effect
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <NavbarMobile />
